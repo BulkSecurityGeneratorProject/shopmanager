20d2670a2b984964614deca997eba0be152cbd64
RekZidik <lyazidi.rezki@gmail.com>
2017-12-30 12:45:51 +0100

    Adds "findAfterDone" methods.

--- src/main/java/com/informatix/shopmanager/repository/TransactionRepository.java
+++ src/main/java/com/informatix/shopmanager/repository/TransactionRepository.java
@@ -3,10 +3,12 @@ package com.informatix.shopmanager.repository;
 import com.informatix.shopmanager.domain.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -22,4 +24,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     @Query("select transaction from Transaction transaction where transaction.user.login = ?#{principal.username}")
     Page<Transaction> findByUserIsCurrentUser(Pageable pageable);
 
+    @Query("select transaction from Transaction transaction where transaction.done > :fromDate")
+    List<Transaction> findAfterDone(@Param("fromDate") LocalDate from);
 }
